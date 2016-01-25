f5041be1038bfce313f2730d2051d6bf4dc5a85c
SunDoge <384813529@qq.com>
2016-01-25 08:31:31 +0800

    test

---
+++ test/cache/69ffa62bde6a3f6f668e490269f22f2075922bd1.test.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-01-24 22:19:33
+  from "D:\wamp\www\mvc\test\tpl\test.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_56a53fe59c7f64_18644509',
+  'file_dependency' => 
+  array (
+    '69ffa62bde6a3f6f668e490269f22f2075922bd1' => 
+    array (
+      0 => 'D:\\wamp\\www\\mvc\\test\\tpl\\test.tpl',
+      1 => 1453670370,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 120,
+),true)) {
+function content_56a53fe59c7f64_18644509 ($_smarty_tpl) {
+?>
+Title<?php }
+}
---
+++ test/template_c/69ffa62bde6a3f6f668e490269f22f2075922bd1_0.file.test.tpl.cache.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-01-24 22:19:33
+  from "D:\wamp\www\mvc\test\tpl\test.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_56a53fe599cbb8_48260173',
+  'file_dependency' => 
+  array (
+    '69ffa62bde6a3f6f668e490269f22f2075922bd1' => 
+    array (
+      0 => 'D:\\wamp\\www\\mvc\\test\\tpl\\test.tpl',
+      1 => 1453670370,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_56a53fe599cbb8_48260173 ($_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '3267856a53fe5803940_70272761';
+echo $_smarty_tpl->tpl_vars['articletitle']->value;
+}
+}
